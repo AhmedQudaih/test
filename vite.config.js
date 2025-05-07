24c23621f4d4e07eccc2d8c9447ab041bea229ac
Ahmed Qudaih <ahmedku0123@gmail.com>
2025-05-08 02:55:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  base: '/Bluefox-ReactJS-free-waitlist-template/',
+  base: '/test/',
   
 })
